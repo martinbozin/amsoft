6c02810a752b070e95226566e2c49c2c0bb99f17
martin bozinovski <martin.bozinovski@orcion.com>
2017-01-24 21:41:24 +0100

    themes done

--- src/CloudOffice/AMSoft.CloudOffice.Data/CloudOfficeDbContext.cs
+++ src/CloudOffice/AMSoft.CloudOffice.Data/CloudOfficeDbContext.cs
@@ -18,6 +18,7 @@ namespace AMSoft.CloudOffice.Data
             modelBuilder.Entity<AppTenant>().HasKey("AppTenantId");
             modelBuilder.Entity<AppTenant>().Property("Name");
             modelBuilder.Entity<AppTenant>().Property("Hostname");
+            modelBuilder.Entity<AppTenant>().Property("Theme");
             modelBuilder.Entity<AppTenant>().Property("ConnectionString");
             modelBuilder.Entity<AppTenant>().Ignore(e => e.Hostnames);
         }
