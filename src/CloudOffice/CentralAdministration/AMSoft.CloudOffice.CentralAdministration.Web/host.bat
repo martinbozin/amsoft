89f865d38a41df7f094cc617bd5d2b5d1696c766
durmisi <durmisi@gmail.com>
2017-01-28 23:57:07 +0100

    update nuget packages

---
+++ src/CloudOffice/CentralAdministration/AMSoft.CloudOffice.CentralAdministration.Web/host.bat
@@ -0,0 +1,3 @@
+﻿@echo "Run CloudOffice Public Web"
+ set ASPNETCORE_ENVIRONMENT=Development
+dotnet "C:\Projects\amsoft\src\CloudOffice\CentralAdministration\AMSoft.CloudOffice.CentralAdministration.Web\bin\Debug\netcoreapp1.1\AMSoft.CloudOffice.CentralAdministration.Web.dll"
---
+++ src/CloudOffice/Dashboard/AMSoft.CloudOffice.Dashboard.Web/host.bat
@@ -0,0 +1,3 @@
+﻿@echo "Run AMSoft.CloudOffice.Dashboard.Web"
+ set ASPNETCORE_ENVIRONMENT=Development
+dotnet "C:\Projects\amsoft\src\CloudOffice\Dashboard\AMSoft.CloudOffice.Dashboard.Web\bin\Debug\netcoreapp1.1\AMSoft.CloudOffice.Dashboard.Web.dll"
