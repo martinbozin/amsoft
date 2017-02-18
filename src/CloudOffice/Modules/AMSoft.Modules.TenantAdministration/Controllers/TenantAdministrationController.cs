21ab523ccd6894eb315963c67d7944c07fc03b92
martin bozinovski <martin.bozinovski@orcion.com>
2017-02-18 16:39:58 +0100

    Add tenant admin module

---
+++ src/CloudOffice/Modules/AMSoft.Modules.TenantAdministration/Controllers/TenantAdministrationController.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+using ExtCore.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AMSoft.Modules.TenantAdministration.Controllers
+{
+  public class TenantAdministrationController : Controller
+  {
+    public ActionResult Index()
+    {
+      return this.View(ExtensionManager.Extensions.Select(e => e.Name));
+    }
+  }
+}
